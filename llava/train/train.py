aa4f173592f7840bd0d04ddd9486f40775ae34d2
Joseph Lee <jiosephlee@gmail.com>
2025-10-30 03:41:00 -0400

    fix read dataset function

--- llava/train/train.py
+++ llava/train/train.py
@@ -680,11 +680,10 @@ def preprocess(
 class LazySupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, data_path: str,
-                 tokenizer: transformers.PreTrainedTokenizer,
-                 data_args: DataArguments):
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer,
+                 data_args: DataArguments,
+                 list_data_dict: list):
         super(LazySupervisedDataset, self).__init__()
-        list_data_dict = json.load(open(data_path, "r"))
 
         rank0_print("Formatting inputs...Skip in lazy mode")
         self.tokenizer = tokenizer
